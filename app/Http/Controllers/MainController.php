7a1efc9202b46e73584ca7928ba0de122c0d3d11
emilie <emilievergauwe@gmail.com>
2022-05-03 21:32:50 +0200

    create open and closed task lists

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,8 +13,28 @@ class MainController extends Controller
 
     public function userDashboard() {
         $company = "ScenoPhoto";
+
+        # Query database to get all current company tasks
         $companyTasks = DB::select('select * from tasks where company = "ScenoPhoto"');
-        dd($companyTasks);
-        return view('todo-users');
+
+        # Convert tasks object to array
+        $tasks = json_decode(json_encode($companyTasks), true);
+
+        # Iterate through tasks and create one array for open tasks and one for closed tasks
+        $openTasks = [];
+        $closedTasks = [];
+        foreach($tasks as $task) {
+            if ($task['status'] == "open") {
+                array_push($openTasks, $task);
+            }
+            else {
+                array_push($closedTasks, $task);
+            }
+        }
+
+        return view('todo-users', [
+            'opentasks' => $openTasks,
+            'closedTasks' => $closedTasks
+        ]);
     }
 }
